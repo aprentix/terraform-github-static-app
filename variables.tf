b82495e25f5ade8260f661672ce975f5782a58fa
aprentix <raqueldiazchavez9@gmail.com>
2025-03-06 13:11:53 +0100

    example

--- variables.tf
+++ variables.tf
@@ -1,34 +1,31 @@
 variable "template_org" {
   description = "The name of the organization in Github that will contain the example app repo."
-  default     = "hashicorp-education"
+  default     = "aprentix"
 }
 
 variable "template_repo" {
   description = "The name of the repository in Github that contains the example app code."
-  default     = "learn-hcp-waypoint-static-app-template"
+  default     = "terraform-github-static-app"
 }
 
 variable "destination_org" {
   description = "The name of the organization in Github that will contain the templated repo."
-  default     = "hashicorp-education"
+  default     = "aprentix"
 }
 
 variable "gh_token" {
   description = "Github token with permissions to create and delete repos."
+  default = "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"
   sensitive   = true
 }
 
 variable "slack_hook_url" {
   description = "The Slack webhook URL for publishing messages."
-  default     = ""
+  default     = "https://hooks.slack.com/services/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"
 }
 
 variable "waypoint_application" {
   type        = string
-  description = "Name of the Waypoint application."
-
-  validation {
-    condition     = !contains(["-", "_"], var.waypoint_application)
-    error_message = "waypoint_application must not contain dashes or underscores."
-  }
+  description = "terraform-github-static-app"
+  default = "terraform-github-static-app"
 }
